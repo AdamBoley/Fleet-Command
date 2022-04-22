06329b74e46564e06258e745cb126914a92ac41b
AdamBoley <adamboley92@gmail.com>
2022-04-22 20:04:53 +0000

    begin writing mission_one function, refactor excessive print statements in player_fleet_status to use loops

--- run.py
+++ run.py
@@ -10,6 +10,14 @@ player_ships = {
     'destroyers': 150,
 }
 
+player_losses = {
+    'battleships': 0,
+    'battlecruisers': 0,
+    'heavy cruisers': 0,
+    'light cruisers': 0,
+    'destroyers': 0
+}
+
 ship_firepower = {
     'battleship': 20,
     'battlecruiser': 15,
@@ -47,6 +55,25 @@ marines = (
     + (player_ships['battlecruisers'] * 40)
 )
 
+mines = (
+    (player_ships['battleships'] * 10)
+    + (player_ships['battlecruisers'] * 6)
+)
+
+missiles = (
+    (player_ships['battleships'] * 20)
+    + (player_ships['battlecruisers'] * 15)
+    + (player_ships['heavy cruisers'] * 5)
+)
+
+fleet_assets = {
+    'Marines': marines,
+    'Mines': mines,
+    'Missiles': missiles
+}
+
+experience = 1
+
 
 def new_game(player_name):
     """
@@ -70,37 +97,72 @@ def new_game(player_name):
     else:
         print('Indeed - a good naval officer should know the capabilities of their ships by heart')
     
+    mission_one(player_name)
+
+
+def mission_one(player_name):
+    """
+    Mission one - light combat, simple decisions, intended as an introduction to the game
+    """
+    enemy_group_one = {
+        'battleships': 4,
+        'battlecruisers': 4,
+        'heavy cruisers': 12,
+        'light cruisers': 20,
+        'destroyers': 50
+    }
+
+    print(f'Admiral {player_name}, sensors have detected a group of enemy warships at the jump point!')
+    print('The tactical suite is updating now - this group looks small')
+
+    for key, value in enemy_group_one.items():
+        print(f'The enemy have {value} {key}')
+    print('Based on that, I assess this is a scouting unit')
+    print('Admiral, if we choose, we should be able to take them easily!')
+    print('Admiral, what are you orders?')
+    engage_decision_mission_one = input('Press y to engage the enemy, or n to find worthier prey:\n')
+    if engage_decision_mission_one == 'y':
+        print('You: We engage! All hands - battle stations!')
+        print('Roth: How should we approach this engagement?')
+        print('You: We have several options')
+
+    elif engage_decision_mission_one == 'n':
+        print('You: This is not worth our time. Disengage')
+
+    
 
 def player_fleet_status():
     """
     Function that can be called anytime to display the current status of the player's fleet
     """
-    print(f"We currently have {player_ships['battleships']} battleships")
-    print(f"We currently have {player_ships['battlecruisers']} battlecruisers")
-    print(f"We currently have {player_ships['heavy cruisers']} heavy cruisers")
-    print(f"We currently have {player_ships['light cruisers']} light cruisers")
-    print(f"We currently have {player_ships['destroyers']} destroyers")
+    for key, value in player_ships.items():
+        print(f'We currently have {value} {key}')
     print(f"Given the current number of ships, we currently have {total_firepower} turrets")
     print(f"We currently have {total_crew} crew")
-    print(f"We currently have {marines} marines")
+    for key, value in fleet_assets.items():
+        print((f'We currently have {value} {key}'))
 
 
 def ship_capabilities():
     """
     Function that displays the capabilities of each class of ship
     """
-    print('Battleships are the monsters of space combat - heavily armoured and heavily armed.')
-    print(f"Both Alliance and Syndicate battleships have {ship_firepower['battleship']} particle beam turrets\n")
-    print('Battlecruisers are the cavalry of space combat - faster, but less heavily armoured and armed that battleships')
-    print(f"Both Alliance and Syndicate battlecruisers have {ship_firepower['battlecruiser']} turrets\n")
+    print('Battleships are the monsters of space combat - heavily armoured and heavily armed')
+    print(f"Battleships have {ship_firepower['battleship']} particle beam turrets")
+    print('Battleships carry a large number of missiles and mines\n')
+    print('Battlecruisers are the cavalry of space combat - faster, but are less heavily armoured and armed that battleships')
+    print(f"Battlecruisers have {ship_firepower['battlecruiser']} turrets")
+    print('Battlecruisers carry a large number of missiles and mines\n')
+    print("Battleships and battlecruisers carry the fleets' Marines\n")
     print('Heavy Cruisers are the largest escorts')
-    print(f"Heavy cruisers have {ship_firepower['heavy cruiser']} turrets\n")
+    print(f"Heavy cruisers have {ship_firepower['heavy cruiser']} turrets")
+    print('Heavy cruisers also carry a small number of missiles\n')
     print('Light cruisers are mid-weight escorts')
     print(f"Light cruisers have {ship_firepower['light cruiser']} turrets\n")
-    print('Destroyers are the lightest, least capable escorts, but can be effective in large numbers.')
+    print('Destroyers are the lightest, least capable escorts, but can be effective in large numbers')
     print(f"Destroyers have {ship_firepower['destroyer']} turrets\n")
-    print('Crew are used for operating ships.')
-    print("Marines are used for boarding enemy ships and conducting ground assaults.")
+    print('Crew are used for operating ships')
+    print("Marines are used for boarding enemy ships and conducting ground assaults")
 
 
 def main():
